1ce93a39482e00845095def0c72ae9fa8821eda7
HenryCROSS <henry583050973@gmail.com>
2023-04-23 15:42:02 -0400

    up to 15.2.1

--- src/vm.h
+++ src/vm.h
@@ -2,11 +2,16 @@
 #define CLOX_VM_H
 
 #include "chunk.h"
+#include "value.h"
+
+#define STACK_MAX 256
 
 typedef struct
 {
     Chunk* chunk;
     uint8_t* ip; // instruction pointer, JVM call it "PC" stands for program counter
+    Value stack[STACK_MAX];
+    Value* stackTop;
 } VM;
 
 typedef enum
@@ -19,5 +24,7 @@ INTERPRET_RUNTIME_ERROR
 void initVM();
 void freeVM();
 InterpretResult interpret(Chunk* chunk);
+void push(Value value);
+Value pop();
 
 #endif // CLOX_VM_H
