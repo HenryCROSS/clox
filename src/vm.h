b8c69118bd9e02a0661676ba22ec48f6ca80b053
HenryCROSS <henry583050973@gmail.com>
2023-04-24 15:48:02 -0400

    finish 16.2.0

--- src/vm.h
+++ src/vm.h
@@ -23,7 +23,7 @@ INTERPRET_RUNTIME_ERROR
 
 void initVM();
 void freeVM();
-InterpretResult interpret(Chunk* chunk);
+InterpretResult interpret(const char* chunk);
 void push(Value value);
 Value pop();
 
