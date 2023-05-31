be2d97cd3c9c0b017f7b99730f83ca3696551524
HenryCROSS <henry583050973@gmail.com>
2023-05-30 21:44:00 -0400

    done  18.3.0

--- src/value.h
+++ src/value.h
@@ -22,8 +22,8 @@ typedef struct {
 #define IS_NIL(value) ((value).type == VAL_NIL)
 #define IS_NUMBER(value) ((value).type == VAL_NUMBER)
 
-#define AS_BOOL(value) ((vlaue).as.boolean)
-#define AS_NUMBER(value) ((vlaue).as.number)
+#define AS_BOOL(value) ((value).as.boolean)
+#define AS_NUMBER(value) ((value).as.number)
 
 #define BOOL_VAL(value) ((Value){VAL_BOOL, {.boolean = value}})
 #define NIL_VAL(value) ((Value){VAL_NIL, {.number = value}})
