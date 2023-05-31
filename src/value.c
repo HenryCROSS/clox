115c6fc41cf4afcefb8646feec11cb6385ea60d6
HenryCROSS <henry583050973@gmail.com>
2023-05-31 00:16:14 -0400

    done 18.4.0

--- src/value.c
+++ src/value.c
@@ -12,12 +12,11 @@ void initValueArray(ValueArray *array)
 
 void writeValueArray(ValueArray *array, Value value)
 {
-    if(array->capacity < array->count + 1)
+    if (array->capacity < array->count + 1)
     {
         int oldCapacity = array->capacity;
         array->capacity = GROW_CAPACITY(oldCapacity);
-        array->values = GROW_ARRAY(Value, array->values,
-                                   oldCapacity, array->capacity);
+        array->values = GROW_ARRAY(Value, array->values, oldCapacity, array->capacity);
     }
 
     array->values[array->count] = value;
@@ -32,5 +31,16 @@ void freeValueArray(ValueArray *array)
 
 void printValue(Value value)
 {
-    printf("%g", AS_NUMBER(value));
+    switch (value.type)
+    {
+    case VAL_BOOL:
+        printf(AS_BOOL(value) ? "true" : "false");
+        break;
+    case VAL_NIL:
+        printf("nil");
+        break;
+    case VAL_NUMBER:
+        printf("%g", AS_NUMBER(value));
+        break;
+    }
 }
