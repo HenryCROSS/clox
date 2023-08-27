77db55055cb91d7ca0ed31cc35bd139b1adc744d
HenryCROSS <henry583050973@gmail.com>
2023-08-27 16:34:48 -0400

    fix issue

--- src/vm.c
+++ src/vm.c
@@ -92,21 +92,20 @@ static InterpretResult run()
 #define READ_SHORT() (frame->ip += 2, (uint16_t)((frame->ip[-2] << 8) | frame->ip[-1]))
 #define READ_CONSTANT() (frame->function->chunk.constants.values[READ_BYTE()])
 #define READ_STRING() AS_STRING(READ_CONSTANT())
-#define BINARY_OP(valueType, op)
-
-    do
-    {
-        if (!IS_NUMBER(peek(0)) || !IS_NUMBER(peek(1)))
-        {
-            runtimeError("Operands must be numbers.");
-            return INTERPRET_RUNTIME_ERROR;
-        }
-        double b = AS_NUMBER(pop());
-        double a = AS_NUMBER(pop());
-        push(valueType(a op b));
+#define BINARY_OP(valueType, op)                                                                                       \
+    do                                                                                                                 \
+    {                                                                                                                  \
+        if (!IS_NUMBER(peek(0)) || !IS_NUMBER(peek(1)))                                                                \
+        {                                                                                                              \
+            runtimeError("Operands must be numbers.");                                                                 \
+            return INTERPRET_RUNTIME_ERROR;                                                                            \
+        }                                                                                                              \
+        double b = AS_NUMBER(pop());                                                                                   \
+        double a = AS_NUMBER(pop());                                                                                   \
+        push(valueType(a op b));                                                                                       \
     } while (false)
 
-        for (;;)
+    for (;;)
     {
 #ifdef DEBUG_TRACE_EXECUTION
         printf("          ");
