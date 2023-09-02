09fead0e9a6ab583d4e299dd6abfd86e90c63c50
HenryCROSS <henry583050973@gmail.com>
2023-09-02 15:27:34 -0400

    25.1.1

--- src/compiler.c
+++ src/compiler.c
@@ -664,6 +664,7 @@ static void function(FunctionType type)
 
     ObjFunction *function = endCompiler();
     emitBytes(OP_CONSTANT, makeConstant(OBJ_VAL(function)));
+    emitBytes(OP_CLOSURE, makeConstant(OBJ_VAL(function)));
 }
 
 static void funDeclaration()
