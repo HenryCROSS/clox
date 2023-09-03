ae9e3014afc29b5ee1071c69eed1fb84ef9df157
HenryCROSS <henry583050973@gmail.com>
2023-09-02 21:15:36 -0400

    fix bug

--- src/compiler.c
+++ src/compiler.c
@@ -720,7 +720,6 @@ static void function(FunctionType type)
     block();
 
     ObjFunction *function = endCompiler();
-    emitBytes(OP_CONSTANT, makeConstant(OBJ_VAL(function)));
     emitBytes(OP_CLOSURE, makeConstant(OBJ_VAL(function)));
 
     for (int i = 0; i < function->upvalueCount; i++)
