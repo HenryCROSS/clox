aa3d935ef21b7783a32c8df43921379f83fd7c53
HenryCROSS <henry583050973@gmail.com>
2024-02-26 15:33:08 -0500

    26.7.2

--- src/object.c
+++ src/object.c
@@ -59,7 +59,11 @@ static ObjString *allocateString(char *chars, int length, uint32_t hash) {
   string->chars = chars;
   string->length = length;
   string->hash = hash;
+
+  push(OBJ_VAL(string));
   tableSet(&vm.strings, string, NIL_VAL);
+  pop();
+
   return string;
 }
 
